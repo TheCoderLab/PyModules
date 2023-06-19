22af9bb177dea1aebdf35ca9151161134983c6ca
TheCoderLab <rudrasinghrathore16022012@gmail.com>
2023-06-19 18:35:34 +0530

    Created The ThreeToTheOneHalfPy Module. Edited The main.py file.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import squareroot2py
 import pysqrtrootof3taker
+import threetotheonehalfpy
 
 print('Square Root Of 2: ' + str(squareroot2py.compute_square_root_of_2()))
-print('Square Root Of 3: ' + str(pysqrtrootof3taker.compute_square_root_of_3()))
+print('Square Root Of 3 (Using PySqrtRootOf3Taker): ' + str(pysqrtrootof3taker.compute_square_root_of_3()))
+print('Square Root Of 3 (Using ThreeToTheOneHalfPy): ' + str(threetotheonehalfpy.compute_three_to_the_one_half()))
---
+++ threetotheonehalfpy.py
@@ -0,0 +1,2 @@
+def compute_three_to_the_one_half():
+    return 3 ** (1/2)
