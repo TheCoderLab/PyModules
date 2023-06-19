5b1c9f35f88f10cc7018e8f26f421563e57cb22c
TheCoderLab <rudrasinghrathore16022012@gmail.com>
2023-06-19 18:27:09 +0530

    Created the PySqrtOf3Taker Module. Updated The Main.py file.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import squareroot2py
+import pysqrtrootof3taker
 
-print(squareroot2py.compute_square_root_of_2())
+print('Square Root Of 2: ' + str(squareroot2py.compute_square_root_of_2()))
+print('Square Root Of 3: ' + str(pysqrtrootof3taker.compute_square_root_of_3()))
